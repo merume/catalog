a634303ee34ec4c4136e11cf483ab6044be93a7f
Masaki Sato <merume@gmail.com>
2013-02-18 00:01:52 +0900

    [#44602617] Add scenario about show catalog

--- spec/decorators/product_decorator_spec.rb
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-require 'spec_helper'
-
-describe ProductDecorator do
-  let(:product) { Product.new.extend ProductDecorator }
-  subject { product }
-  it { should be_a Product }
-end
